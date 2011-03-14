8b1056bae293bd774642e9d119ae3109964f2432
Joseph Kingry <jkingry@gmail.com>
2011-03-14 16:23:16 -0400

    problem 1 w/ basic understanding of pattern matching

--- haskell/1/1.hs
+++ haskell/1/1.hs
@@ -1,10 +1,9 @@
 p1 n = p1a (n - 1)
 
-p1a n = if n == 0
-	then 0
-	else if (mod n 3) == 0 || (mod n 5) == 0
-		then n + p1a (n - 1)
-		else p1a (n - 1)
+p1a 0 = 0
+p1a n = if (mod n 3) == 0 || (mod n 5) == 0
+       then n + p1a (n - 1)
+       else p1a (n - 1)
 
 main = print (p1 1000)
 
