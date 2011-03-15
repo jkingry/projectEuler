319b4588514bf3ca061629f1d5b89d90d141b8e3
Joseph Kingry <jkingry@gmail.com>
2011-03-15 11:37:10 -0400

    chapter 3 work

---
+++ haskell/learning/chap3.hs
@@ -0,0 +1,24 @@
+-- exercise 1,2
+listLen :: [a] -> Integer
+listLen [] = 0
+listLen (x:xs) = 1 + listLen xs
+
+-- exercise 3
+mean list = (sum list) / fromIntegral (length list)
+
+-- exercise 4
+palin [] = []
+palin [a] = [a]
+palin (a:b) = a:((palin b) ++ [a])
+
+-- exercise 5
+isPalindrome list = list == reverse list
+
+-- exercise 6 ??
+-- sortByLength list = sortBy length list
+
+-- exercise 7
+inter:: a -> [[a]] -> [a]
+inter sep [] = []
+inter sep [x] = x
+inter sep (x:xs) = x ++ (sep:(inter sep xs))
