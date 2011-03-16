193bb53719e1941e2ece4d5d651ed12c5ed836e4
Joseph Kingry <jkingry@gmail.com>
2011-03-16 16:25:21 -0400

    problem 2 using lambda

--- haskell/2/2.hs
+++ haskell/2/2.hs
@@ -2,7 +2,6 @@ fib n = fibs!!n
 
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
-p2 limit = sum (filter even (takeWhile lessThenLimit fibs))
-	where lessThenLimit x = x < limit
+p2 limit = sum (filter even (takeWhile (\n -> n < limit) fibs))
 
 main = print (p2 4000000)
