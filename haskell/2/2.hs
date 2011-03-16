d34db4946e4093e9625a3da167ee97a96dbc8427
Joseph Kingry <jkingry@gmail.com>
2011-03-16 16:44:49 -0400

    curry the limit function

--- haskell/2/2.hs
+++ haskell/2/2.hs
@@ -1,5 +1,5 @@
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
-p2 limit = sum (filter even (takeWhile (\n -> n < limit) fibs))
+p2 limit = sum (filter even (takeWhile (<limit) fibs))
 
 main = print (p2 4000000)
