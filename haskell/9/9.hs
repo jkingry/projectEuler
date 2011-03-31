2e8c5c7c4f98956c0819771fbce0479bdc852e64
Joseph Kingry <jkingry@gmail.com>
2011-03-30 21:32:09 -0400

    solved 9, algebraic simplification mainly

---
+++ haskell/9/9.hs
@@ -0,0 +1,35 @@
+bruteForceTriplet = [ (a,b,c) | 
+	c <- [1..998],
+	a <- [1..c],
+	b <- [1..a],
+	a^2 + b^2 == c^2,
+	a + b + c == 1000]
+
+{-
+Simplification of equations
+
+a^2 + b^2 = c^2
+a + b + c = 1000
+
+c = 1000 - (a+b)
+
+a^2 + b^2 = ((1000 - (a+b))^2
+
+a^2 + b^2 = 1000000 - 2000a - 2000b + 2ab + a^2 + b^2
+
+0 = 1000000 - 2000a - 2000b + 2ab
+
+500000 = 1000a + 1000b - ab 
+-}
+
+solution = [ (a,b) | 
+	a <- [1..998],
+	b <- [1..a],
+	500000 == 1000*a + 1000*b - a*b]
+
+(a,b) = head solution
+c = 1000 - (a+b)
+
+p9 = a*b*c
+
+main = print p9
