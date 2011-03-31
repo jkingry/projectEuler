dc8fa3080eadfebc644ec8f9ea08694385c49ea6
Joseph Kingry <jkingry@gmail.com>
2011-03-30 21:40:04 -0400

    solved 10, just brute force sum of existing prime function

---
+++ haskell/10/10.hs
@@ -0,0 +1,12 @@
+primes = 2 : oddprimes
+oddprimes = 3 : sieve oddprimes 3 0
+sieve (p:ps) x k = 
+	[n | 
+		n <- [x+2,x+4..p*p-2], 
+		and [rem n q /= 0 | q <- take k oddprimes]]
+	++
+	sieve ps (p*p) (k+1)
+
+p10 = sum $ takeWhile (<2000000) primes
+
+main = print p10
