59e99b4b73387e689fe7892e5924a4c39229e062
Joseph Kingry <jkingry@gmail.com>
2011-04-01 13:59:55 -0400

    P14 solved with C#

---
+++ csharp/P14/P14.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P14
+{
+    internal static class Program
+    {
+        static Dictionary<long, int> lengths = new Dictionary<long, int>();
+
+        static long Colltaz(long n)
+        {
+            if (n % 2 == 0)
+            {
+                return n / 2;
+            }
+            else
+            {
+                return 3 * n + 1;
+            }
+        }
+
+        static int CollatzLength(long n)
+        {
+            if (n <= 1) return 1;
+
+            if (lengths.ContainsKey(n))
+            {
+                return lengths[n];
+            }
+
+            var length = 1 + CollatzLength(Colltaz(n));
+            lengths.Add(n, length);
+
+            return length;
+        }
+
+        static Tuple<long, int> MaxCollatzLength(long n)
+        {
+            if (n <= 1) return Tuple.Create(1L,1);
+
+            var m = 1L;
+            var mL = 1;
+            for (var i = 0L; i <= n; ++i)
+            {
+                var L = CollatzLength(i);
+                if (L >= mL)
+                {
+                    mL = L;
+                    m = i;
+                }
+            }
+
+            return Tuple.Create(m, mL);
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(MaxCollatzLength(1000000).Item1);
+        }
+    }
+}
