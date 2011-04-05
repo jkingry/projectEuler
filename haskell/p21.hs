a184b0f64a1ca1ce708fc331b2de452696293778
Joseph Kingry <jkingry@shoplogix.com>
2011-04-05 13:44:42 -0400

    fixed problem 21?

--- haskell/p21.hs
+++ haskell/p21.hs
@@ -3,17 +3,6 @@ module P21 (p21, p21Test) where
 import EulerCommon
 
 import Data.List
-import Math.Sieve.Factor
-import qualified Data.Set as Set
-
-q = sieve 32000
-
-distinct = Set.toList . Set.fromList
-
-factors n = map product $ distinct $ subsequences $ concat $ map r $ factor q n
-    where r (p,n) = genericReplicate n p
-
-strictFactors n = filter (<n) $ factors n
    
 fp21 n = sum $ filter isAmicable [2..n]
 
